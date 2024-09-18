18a161e7cbb82b662e9a535675ec605a4d619be7
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-09-18 15:40:19 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application; // Define o pacote onde a classe Program está contida
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,7 @@ import model.entities.Seller;     // Importa a classe Seller do pacote model.ent
 public class Program {
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();// implemetando a instaciação da interface.
 		
@@ -47,5 +49,12 @@ public class Program {
 		sellerDao.update(seller);
 		
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deletById(id);
+		System.out.println("Delete completed");
+		sc.close();
 	}
 }
